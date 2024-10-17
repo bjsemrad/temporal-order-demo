829cf2cdd0e43713d8f1b61b37a333dfb3d9fc24
Brian Semrad <bjsemrad@gmail.com>
2024-10-16 20:47:58 -0500

    Clean up

--- pkg/order-workflow/workflow.go
+++ pkg/order-workflow/workflow.go
@@ -18,19 +18,23 @@ func ProcessOrder(ctx workflow.Context, custOrder *order.Order) (order.Order, er
 
 	log.Printf("Start Ensuring Order is Priced: %s.\n", custOrder.OrderNumber)
 	orderworkflowstep.EnsureOrderIsPriced(ctx, custOrder)
+
 	log.Printf("Start Fraud Check: %s.\n", custOrder.OrderNumber)
 	fraudErr := orderworkflowstep.StartFraudCheck(ctx, custOrder)
-	log.Printf("End Fraud Check Step: %s.\n", custOrder.OrderNumber)
 	var fraudDetectedError *orderworkflowstep.FraudDetectedError
 	if fraudErr != nil && !errors.As(fraudErr, &fraudDetectedError) {
 		return *custOrder, fraudErr
 	}
+	log.Printf("End Fraud Check Step: %s.\n", custOrder.OrderNumber)
+
 	log.Printf("Start Validate Business Rules Check Step: %s.\n", custOrder.OrderNumber)
 	//TODO: Validate Business Rules
 	log.Printf("End Validate Business Rules Step: %s.\n", custOrder.OrderNumber)
+
 	log.Printf("Start Cust Approval Step: %s.\n", custOrder.OrderNumber)
 	//TODO: Cust Approval
 	log.Printf("End Cust Approval Step: %s.\n", custOrder.OrderNumber)
+
 	if custOrder.Payment != nil && strings.Trim(custOrder.Payment.AccountNumber, " ") != "" {
 		log.Printf("Start Credit Review Step: %s.\n", custOrder.OrderNumber)
 		creditReviewErr := orderworkflowstep.StartCreditReview(ctx, custOrder)
@@ -41,22 +45,24 @@ func ProcessOrder(ctx workflow.Context, custOrder *order.Order) (order.Order, er
 		log.Printf("End Credit Review Step: %s.\n", custOrder.OrderNumber)
 
 	}
+
 	log.Printf("Start Apply Setting Step: %s.\n", custOrder.OrderNumber)
+	settingsErr := orderworkflowstep.ApplySettings(ctx, custOrder)
+	if settingsErr != nil {
+		return *custOrder, settingsErr
+	}
+	log.Printf("End Apply Setting Step: %s.\n", custOrder.OrderNumber)
+
+	log.Printf("Start Ops Rules Step: %s.\n", custOrder.OrderNumber)
+	//TODO: Operational Rule Checks
+	log.Printf("End Ops Rules Step: %s.\n", custOrder.OrderNumber)
+
+	log.Printf("Start Intervention Step: %s.\n", custOrder.OrderNumber)
+	//TODO: Team Intervention
+	log.Printf("End Intervention Step: %s.\n", custOrder.OrderNumber)
+
 	//IF we are not in a terminal status send the fulfillment signal
 	if !orderstatus.TerminalOrderStatus(custOrder.Status.Code) {
-		settingsErr := orderworkflowstep.ApplySettings(ctx, custOrder)
-		if settingsErr != nil {
-			return *custOrder, settingsErr
-		}
-		log.Printf("End Apply Setting Step: %s.\n", custOrder.OrderNumber)
-
-		log.Printf("Start Ops Rules Step: %s.\n", custOrder.OrderNumber)
-		//TODO: Operational Rule Checks
-		log.Printf("End Ops Rules Step: %s.\n", custOrder.OrderNumber)
-		log.Printf("Start Intervention Step: %s.\n", custOrder.OrderNumber)
-		//TODO: Team Intervention
-		log.Printf("End Intervention Step: %s.\n", custOrder.OrderNumber)
-
 		log.Printf("Start Prepare For Fulfillment Step: %s.\n", custOrder.OrderNumber)
 		fulfillError := orderworkflowstep.PrepareOrderForFulfillment(ctx, custOrder)
 		if fulfillError != nil {
@@ -80,7 +86,7 @@ func setupDefaultContext(ctx workflow.Context) workflow.Context {
 	}
 
 	options := workflow.ActivityOptions{
-		// Timeout options specify when to automatically timeout Activity functions.
+		// Timeout options specify when to automatically timeout Activity functions, this will issue a retry after 5 minutes
 		StartToCloseTimeout: 5 * time.Minute,
 		RetryPolicy:         retrypolicy,
 	}
